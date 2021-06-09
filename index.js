050d6cc07bab897f6f158b75881efc2d3dd58144
johnsw87 <johnsw87@gmail.com>
2021-06-09 19:37:48 +0100

    Update index.js
    updated var

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
+  const name = process.env.NAME || 'New World';
   res.send(`Hello ${name}!`);
 });
 
